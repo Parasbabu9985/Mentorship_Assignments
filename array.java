a0c71e80bcdeeb46fbc248e49bcd214433fbc02d
Parasbabu9985 <132127203+Parasbabu9985@users.noreply.github.com>
2024-10-16 14:50:03 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+ class array {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+
+    // data type arr name = new data type [size];
+
+    int arr[]= new int [5];
+
+    for(int i=0;i<5;i++){
+
+        arr[i]= sc.nextInt();
+        }
+
+        for(int i=0;i<5;i++){
+
+          System.out.println(arr[i]);
+            }
+
+        
+
+
+    }
+}
