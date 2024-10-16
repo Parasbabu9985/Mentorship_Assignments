e46db1f7b9fa1d579511e11c20356eb0d7106d7d
Parasbabu9985 <132127203+Parasbabu9985@users.noreply.github.com>
2024-10-16 14:53:09 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class string {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      String shivi = sc.nextLine();
+
+      System.out.println(shivi);
+      System.out.println("dangaero");
+    }
+}
+
+
+//string jhfr = sc.nextLine;
